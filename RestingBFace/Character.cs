99eae4129fb5caa1bd3444d38e89445b653668e5
Oliver Granudden <oliver.granudden@elev.ga.ntig.se>
2024-01-26 10:15:21 +0100

    initial

---
+++ RestingBFace/Character.cs
@@ -0,0 +1,10 @@
+﻿public class Character
+{
+    public string name { get; set; }
+    public string height { get; set; }
+    public string mass { get; set; }
+    public string hair_color { get; set; }
+    public string skin_color { get; set; }
+    public string eye_color { get; set; }
+    public string gender { get; set; }
+}
---
+++ RestingBFace/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+using RestSharp;
+using System.IO;
+
+RestClient client = new("https://swapi.py4e.com/api/");
+
+while (true)
+{
+    Console.WriteLine("Write a number");
+    string input = Console.ReadLine().ToLower().Trim();
+    RestRequest request = new($"people/{input}/");
+
+    RestResponse response = client.GetAsync(request).Result;
+
+    if(response.StatusCode == System.Net.HttpStatusCode.OK)
+    {
+        Character c = JsonSerializer.Deserialize<Character>(response.Content);
+
+        Console.WriteLine($"Name: {c.name}");
+        Console.WriteLine($"Height: {c.height}");
+        Console.WriteLine($"Mass: {c.mass}");
+        Console.WriteLine($"Hair Color: {c.hair_color}");
+        Console.WriteLine($"Skin Color: {c.skin_color}");
+        Console.WriteLine($"Eye Color: {c.eye_color}");
+        Console.WriteLine($"Gender: {c.gender}");
+    }
+    else Console.WriteLine(response.StatusCode);
+
+    // Console.WriteLine(response.Content);
+    // File.WriteAllText("test.json", response.Content);
+
+    Console.WriteLine();
+}
+
